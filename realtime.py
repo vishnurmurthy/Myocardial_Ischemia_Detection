3b8fd7820ee79636f590531819e94f9f56bfb2ce
Siddarth Ijju <dulluijju@gmail.com>
2018-08-02 13:32:21 -0400

    Fixed realtime.py

--- realtime.py
+++ realtime.py
@@ -1,16 +1,18 @@
 import requests
 import time
 
-print('Starting Experiment')
-# print(requests.get('http://localhost:22002/NeuLogAPI?GetSensorValue:[Pulse],[1]').text)
-#requests.get('http://localhost:22002/NeuLogAPI?GetServerStatus')
-start = time.time()
-requests.get('http://localhost:22002/NeuLogAPI?StartExperiment:[Pulse],[1],[4],[5000]')
-# print(requests.get('http://localhost:22002/NeuLogAPI?GetServerStatus').text)
-print('Finished Experiment in ' + str(time.time() - start) + " seconds")
-# print('Getting Data')
-r =  requests.get('http://localhost:22002/NeuLogAPI?GetExperimentSamples')
-requests.get('http://localhost:22002/NeuLogAPI?StartExperiment:[Pulse],[1],[4],[5000]')
-print(r.text)
-requests.get('http://localhost:22002/NeuLogAPI?StopExperiment')
- 
+def collectData(seconds):
+   # print('Starting Experiment')
+   # start = time.time()
+   # 50 samples per second * seconds
+   samples = 50 * seconds
+   requests.get('http://localhost:22002/NeuLogAPI?StartExperiment:[Pulse],[1],[6],[' + str(samples) + ']')
+   time.sleep(2 * seconds)
+   # print('Finished Experiment in ' + str(time.time() - start) + " seconds")
+   r =  requests.get('http://localhost:22002/NeuLogAPI?GetExperimentSamples')
+   print(r.text)
+   requests.get('http://localhost:22002/NeuLogAPI?StopExperiment')
+
+if __name__ == "__main__":
+   seconds = input("Number of seconds to collect for: ")
+   collectData(int(seconds))
