d3433e6a68f6d20f7649f0f51a8cef76b6d9cbe7
vishnurmurthy <vishnurmurthy@gmail.com>
2018-08-02 15:25:49 -0400

    rm

--- data/largeChunk.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import subprocess
-
-x = open('RECORDS.txt', 'r').read().splitlines()
-x = x[64:]
-for i in x:
-	subprocess.call([sys.executable, 'data_chunking.py', i])
-	print(i, "DONE")
