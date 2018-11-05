513b681dbb6ed2ebe16de01460fc4302705197a7
vishnurmurthy <vishnurmurthy@gmail.com>
2018-11-05 13:00:42 -0500

    v5py

--- machine_learning_model/Model_example_v5.py
+++ machine_learning_model/Model_example_v5.py
@@ -35,7 +35,7 @@ for i in datafiles:
     dat['Signal'][count] = dat['Signal'][count] - np.mean(dat['Signal'][count])
     dat['Signal'][count] = dat['Signal'][count] / np.std(dat['Signal'][count])
   large_data = large_data.append(dat)
-#os.chdir('../..')
+
 
 os.chdir('../processed_data')
 
@@ -48,7 +48,6 @@ for i in range(len(datafiles)):
         large_data = large_data.append(dat)
 os.chdir('../')
 
-# In[5]:
 
 
 
@@ -57,70 +56,24 @@ large_data = large_data[large_data.Label != 'sst']
 
 large_data['Label'] = large_data['Label'].map({'st': 0, 'rtst': 1, 'normal': 2})
 
-'''
-=======
-
->>>>>>> dc4b32158d22a68cdb17985d6d75121e092b090d
-print("# of data rows for ST: ", len(large_data.loc[large_data['Label'] == 0]))
-print("# of data rows for RTST: ", len(large_data.loc[large_data['Label'] == 1]))
-print("# of data rows for Normal: ", len(large_data.loc[large_data['Label'] == 2]))
-
-newx = 0
-for i in large_data['Signal']:
-	if len(i)!= 250:
-		print(len(i))
-		newx+=1
-print("nx", newx)
-<<<<<<< HEAD
-'''
-#convert to ints
-
 large_data['Label'] = large_data['Label'].astype('category').cat.codes
 
-#randomly shuffle dataframe
+
 
 large_data = large_data.sample(frac=1).reset_index(drop=True)
 
-<<<<<<< HEAD
+
 plt.plot(np.arange(0, 250), large_data['Signal'][0])
 
 LEN = large_data.shape[0]
-'''
-for i in range(0, 676483, 67000):
-    plt.figure(figsize = (10, 4))
-    plt.plot(np.arange(0, 250), large_data['Signal'][i])
-    plt.show()
 
 
-# In[9]:
-'''
 
-=======
->>>>>>> dc4b32158d22a68cdb17985d6d75121e092b090d
 y = large_data['Label'].values
 X = []
 for i in large_data['Signal']:
     X.append(i)
 X = np.array(X)
-<<<<<<< HEAD
-'''
-=======
-
->>>>>>> dc4b32158d22a68cdb17985d6d75121e092b090d
-print("Y")
-print(y.shape)
-print("X")
-print(X.shape)
-<<<<<<< HEAD
-'''
-
-# In[12]:
-=======
-
-
-# In[84]:
->>>>>>> dc4b32158d22a68cdb17985d6d75121e092b090d
-
 
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
@@ -128,27 +81,6 @@ y_train = tf.keras.utils.to_categorical(y_train, 3)
 y_train = y_train.astype(np.float32)
 y_val = tf.keras.utils.to_categorical(y_val, 3)
 y_val = y_val.astype(np.float32)
-
-#X_train = X_train.astype(np.float32)
-#X_val = X_val.astype(np.float32)
-<<<<<<< HEAD
-'''
-=======
-
->>>>>>> dc4b32158d22a68cdb17985d6d75121e092b090d
-print("Training")
-print(X_train.shape, y_train.shape)
-print("Validation")
-print(X_val.shape, y_val.shape)
-<<<<<<< HEAD
-'''
-
-# In[13]:
-=======
-
-
-# In[ ]:
->>>>>>> dc4b32158d22a68cdb17985d6d75121e092b090d
 
 
 def ConvBlock(model, layers):
@@ -166,13 +98,13 @@ def DenseBlock(model, layers, neurons = None):
         
 
 
-<<<<<<< HEAD
+
 # In[14]:
 
 
 model = tf.keras.models.Sequential()
 
-=======
+
 # In[ ]:
 
 
@@ -189,7 +121,7 @@ model.add(tf.keras.layers.GaussianDropout(.25))
 model.add(tf.keras.layers.Dense(128, activation='relu'))
 model.add(tf.keras.layers.Dense(3, activation='softmax'))
 '''
->>>>>>> dc4b32158d22a68cdb17985d6d75121e092b090d
+
 model.add(tf.keras.layers.InputLayer(input_shape = (X_train[0].shape[0], 1)))
 
 ConvBlock(model, 3)
@@ -199,11 +131,11 @@ DenseBlock(model, 3, (128, 64, 32))
 model.add(tf.keras.layers.Dense(32, activation = 'relu'))
 model.add(tf.keras.layers.Dense(3, activation = 'softmax'))
 
-<<<<<<< HEAD
+
 model.compile(loss='categorical_crossentropy', optimizer=tf.train.AdamOptimizer(learning_rate=0.001), metrics=['accuracy'])
 model.summary()
 
-=======
+
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
 
@@ -217,7 +149,7 @@ print(X_train[0].shape)
 # In[ ]:
 
 
->>>>>>> dc4b32158d22a68cdb17985d6d75121e092b090d
+
 ################################################################
 ############## RUN ONLY FOR CONVOLUTION 1D MODELS ##############
 ################################################################
@@ -229,9 +161,9 @@ X_val = X_val.reshape(len(X_val), X_val[0].shape[0], 1)
 ############## RUN ONLY FOR CONVOLUTION 1D MODELS ##############
 ################################################################
 
-<<<<<<< HEAD
+
 model.fit(X_train, y_train, validation_data = (X_val, y_val), verbose = True, epochs=25)
-=======
+
 
 # In[ ]:
 
@@ -243,21 +175,13 @@ model.save("BWSI2018_final_weights.h5")
 model.save_weights("BWSI2018_final_save_weights.h5")
 # In[78]:
 
->>>>>>> dc4b32158d22a68cdb17985d6d75121e092b090d
+
 
 test_pred = pd.DataFrame(model.predict(X_val))
 test_predict = test_pred.idxmax(axis=1)
 test_labels = [ np.where(label==1)[0][0] for label in y_val]
 test_labels_one_hot = pd.DataFrame(y_val)
-<<<<<<< HEAD
-#test_pred.head(5)
-=======
-test_pred.head(5)
 
-
-# In[80]:
-
->>>>>>> dc4b32158d22a68cdb17985d6d75121e092b090d
 
 fpr = {}
 tpr = {}
@@ -277,25 +201,18 @@ plt.show()
 fpr["micro"], tpr["micro"], _ = metrics.roc_curve(test_labels_one_hot.values.ravel(), test_pred.values.ravel())
 roc_auc = metrics.auc(fpr["micro"], tpr["micro"])
 
-<<<<<<< HEAD
-=======
+
+
 
 # In[82]:
 
 
->>>>>>> dc4b32158d22a68cdb17985d6d75121e092b090d
+
 print("ROC: ", roc_auc)
 print("ACC: ", metrics.accuracy_score(test_labels, test_predict))
 cm = metrics.confusion_matrix(test_labels, test_predict)
 
-<<<<<<< HEAD
-=======
-'''
-ROC:  0.8388993698385805
-ACC:  0.6494009475450305
-'''
 
->>>>>>> dc4b32158d22a68cdb17985d6d75121e092b090d
 print(cm/len(test_labels))
 # Show confusion matrix in a separate window
 plt.matshow(cm/len(test_labels), cmap='gray')
@@ -316,25 +233,7 @@ plt.ylabel('True label')
 plt.xlabel('Predicted label')
 plt.show()
 
-<<<<<<< HEAD
-#Save the model
 
 model.save("BWSI2018v4.h5")
-=======
-
-# In[85]:
-
-
-#Save the model
 
 model.save("BWSI2018.h5")
-
-#Delete existing model
-
-#del model
-
-#Load the model
-
-#model = load_model('my_model.h5')
-
->>>>>>> dc4b32158d22a68cdb17985d6d75121e092b090d
